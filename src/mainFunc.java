f251261057af17e9cba99941b9d7afe46802f442
joonboy <wjkang@somansa.com>
2018-10-11 22:10:07 +0900

    test

--- src/mainFunc.java
+++ src/mainFunc.java
@@ -4,6 +4,8 @@ public class mainFunc {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hell World");
+		
+		System.out.println("TEST");
 	}
 
 }
